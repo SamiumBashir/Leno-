29fd67cc12d1da2e5b74a650ee765e1f10b82a35
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-02-26 05:45:09 +0600

    added feature section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
+document.addEventListener('DOMContentLoaded', function () {
+  // Mobile Menu
+  const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
   const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
 
   toggleButton.addEventListener('click', function () {
     mobileMenu.classList.toggle('active');
   });
+
+});
