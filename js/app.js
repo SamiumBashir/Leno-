99f0688c5b1a4178975145f5faff86443a1bbcd3
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-02-26 03:33:47 +0600

    added js file

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
+  const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
+
+  toggleButton.addEventListener('click', function () {
+    mobileMenu.classList.toggle('active');
+  });
